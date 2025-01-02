c6d9d291213493cb21fdc5b160d5ba4c82fc707f
Het patel <115970458+hetpatel203@users.noreply.github.com>
2025-01-02 17:16:47 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Tictactoe from './Components/TicTacToe/Tictactoe';
+function App() {
+  return (
+    <div className="App">
+      <Tictactoe />
+    </div>
+  );
+}
+
+export default App;
